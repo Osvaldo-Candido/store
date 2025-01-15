84601f5db9c241550c25554fbf0515620f578d63
Osvaldo Candido <osvaldomarcosisaaccandido3@gmail.com>
2025-01-15 22:25:34 +0100

    Função de buscar produtos criada

---
+++ src/components/ListProducts/index.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function ListProducts() {
+  return (
+    <View>
+      <Text>Producto 1</Text>
+    </View>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,43 @@
 import { useFocusEffect } from '@react-navigation/native';
-import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { useCallback, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import { supabase } from '../../services/database';
+import ListProducts from '../../components/ListProducts';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
 
+  useFocusEffect(
+    useCallback(() => {
+      let isActive = true;
+      async function getProducts() {
+        try {
+          const { data, error } = await supabase.from('products').select('*');
+          if (isActive) {
+            setProducts(data);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      getProducts();
+
+      return () => {
+        isActive = false;
+      };
+    }, []),
+  );
+
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(product) => String(product.id)}
+        renderItem={({ product }) => <ListProducts />}
+        numColumns={2}
+        columnWrapperStyle={{ justifyContent: 'space-between' }}
+      />
     </View>
   );
 }
