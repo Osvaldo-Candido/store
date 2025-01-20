fb3d12702fe2cded7d4bc02096157cc0cfa01582
Osvaldo Candido <osvaldomarcosisaaccandido3@gmail.com>
2025-01-21 00:56:00 +0100

    criado a logica de avaliação do produto

--- src/pages/SingleProduct/index.js
+++ src/pages/SingleProduct/index.js
@@ -10,14 +10,18 @@ import {
   ProductName,
   ProductPrice,
   TitleDescription,
+  RaiterContainer,
+  BtnRate,
 } from './styles';
 import { useRoute } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 import { supabase } from '../../services/database';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Text } from 'react-native';
 
 export default function SinglePage() {
   const [product, setProduct] = useState(null);
+  const [rate, setRate] = useState(0);
   const route = useRoute();
   const { id } = route.params;
 
@@ -35,7 +39,6 @@ export default function SinglePage() {
         }
 
         setProduct(data);
-        console.log(data);
       } catch (error) {
         console.error('error', error);
       }
@@ -44,6 +47,10 @@ export default function SinglePage() {
     fetchProduct();
   }, [id]);
 
+  function handleRate(index) {
+    setRate(index);
+  }
+
   return (
     <Container>
       <ImageProduct
@@ -51,13 +58,34 @@ export default function SinglePage() {
         width={'100%'}
         height={220}
       />
+
       <DetailsContainer>
         <NameAndPriceCartContainer>
           <NameAndPriceContainer>
-            <ProductPrice>{product?.price}</ProductPrice>
+            <ProductPrice>
+              <Text
+                style={{
+                  fontSize: 15,
+                  margin: 0,
+                  padding: 0,
+                  fontWeight: 'regular',
+                }}
+              >
+                AOA
+              </Text>
+              {product?.price}
+            </ProductPrice>
             <ProductName>{product?.name}</ProductName>
           </NameAndPriceContainer>
           <ButtonCart>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: 'regular',
+              }}
+            >
+              Adicionar ao carrinho
+            </Text>
             <MaterialIcons
               name="add-shopping-cart"
               size={28}
@@ -65,7 +93,17 @@ export default function SinglePage() {
             />
           </ButtonCart>
         </NameAndPriceCartContainer>
-
+        <RaiterContainer>
+          {[1, 2, 3, 4, 5].map((item, index) => (
+            <BtnRate key={item} onPress={() => handleRate(item)}>
+              <MaterialIcons
+                name={rate >= item ? 'star' : 'star-outline'}
+                size={24}
+                color="#222"
+              />
+            </BtnRate>
+          ))}
+        </RaiterContainer>
         <ContainerDescription>
           <TitleDescription>Descrição</TitleDescription>
           <Description>
--- src/pages/SingleProduct/styles.js
+++ src/pages/SingleProduct/styles.js
@@ -1,16 +1,52 @@
 import styled from 'styled-components/native';
-
-export const Container = styled.View``;
+import { SafeAreaView } from 'react-native-safe-area-context';
+export const Container = styled(SafeAreaView)`
+  padding: 5px;
+`;
 export const ImageProduct = styled.Image`
   width: 100%;
-  height: 200px;
+  height: 250px;
 `;
 export const DetailsContainer = styled.View``;
-export const NameAndPriceCartContainer = styled.Text``;
+export const NameAndPriceCartContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+`;
 export const NameAndPriceContainer = styled.View``;
-export const ProductPrice = styled.Text``;
-export const ProductName = styled.Text``;
-export const ButtonCart = styled.TouchableOpacity``;
+export const ProductPrice = styled.Text`
+  font-size: 25px;
+  font-weight: bold;
+`;
+export const ProductName = styled.Text`
+  font-size: 18px;
+  max-width: 120px;
+  color: #474d59;
+  line-height: 24px;
+`;
+export const ButtonCart = styled.TouchableOpacity`
+  flex-direction: row;
+  gap: 5px;
+  background-color: #f7d5d5;
+  padding: 10px;
+  border: 1px solid #f3c1c1;
+  border-radius: 5px;
+`;
 export const ContainerDescription = styled.View``;
-export const TitleDescription = styled.Text``;
-export const Description = styled.Text``;
+export const TitleDescription = styled.Text`
+  font-size: 16px;
+  max-width: 200px;
+  color: #474d59;
+  font-weight: bold;
+`;
+export const Description = styled.Text`
+  font-size: 16px;
+  color: #636973;
+  line-height: 22px;
+`;
+
+export const RaiterContainer = styled.View`
+  flex-direction: row;
+`;
+export const BtnRate = styled.TouchableOpacity``;
